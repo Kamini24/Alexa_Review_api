c76b18a3d6430baed146da60a76ea9162a980b29
Kamini <kamini.jsmith24@gmail.com>
2021-07-26 12:18:29 +0530

    changed server port

--- alexa/alexa-review/api/route/alexaRoute.js
+++ alexa/alexa-review/api/route/alexaRoute.js
@@ -5,7 +5,7 @@ module.exports = function () {
 
     // router.get('/getReview', alexa.getReview);
      router.post('/acceptReview', alexaController.acceptReview);
-     router.post('/acceptReview', alexaController.acceptReview);
+    //  router.post('/acceptReview', alexaController.acceptReview);
     router.get('/fetchReview', alexaController.fetchAAllreview);
     router.get('/getMOnthlyRating/:review_source', alexaController.getMonthlyRatingBystore);
     router.get('/getTotalRating/:rating', alexaController.getTotalRatingByCategory);
