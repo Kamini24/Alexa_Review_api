32e4b8f1fafdc8ad629db4076fea9dd8308a1d3f
Kamini <kamini.jsmith24@gmail.com>
2021-07-27 05:56:37 +0530

    Adding the lightsail db connection

--- alexa/alexa-review/api/route/alexaRoute.test.js
+++ alexa/alexa-review/api/route/alexaRoute.test.js
@@ -2,26 +2,26 @@ jest.mock('../controller/alexa.controller');
 const alexaController = require('../controller/alexa.controller');
 const request = require('supertest');
 
-describe.skip('checking access review', () => {
+describe('checking access review', () => {
     let app, appUrl;
     beforeAll(() => {
         app = require('../server');
         appUrl = 'api/alexa'
     })
-    test('checking /acceptReview route which gets all review would be saved', async () => {
-        await request(app).post(`${appUrl}/acceptReview`).send({});
+    it('checking /acceptReview route which gets all review would be saved', async () => {
+        await request(app).post(`/${appUrl}/acceptReview`).send({});
         expect(alexaController['acceptReview']).toHaveBeenCalledTimes(1);
     })
-    test('checking /fetchReview route which gets all review would be saved', async () => {
-        await request(app).get(`${appUrl}/fetchReview`);
+    it('checking /fetchReview route which gets all review would be saved', async () => {
+        await request(app).get(`/${appUrl}/fetchReview`);
         expect(alexaController['fetchAAllreview']).toHaveBeenCalledTimes(1);
     })
-    test('checking / getMOnthlyRating route which gets all review would be saved', async () => {
-        await request(app).get(`${appUrl}/getMOnthlyRating/iTunes`);
+    it('checking / getMOnthlyRating route which gets all review would be saved', async () => {
+        await request(app).get(`/${appUrl}/getMOnthlyRating/iTunes`);
         expect(alexaController['getMonthlyRatingBystore']).toHaveBeenCalledTimes(1);
     })
-    test('checking / getTotalRating route which gets all review would be saved', async () => {
-        await request(app).get(`${appUrl}/getTotalRating/5`);
+    it('checking / getTotalRating route which gets all review would be saved', async () => {
+        await request(app).get(`/${appUrl}/getTotalRating/5`);
         expect(alexaController['getTotalRatingByCategory']).toHaveBeenCalledTimes(1);
     })
 })
